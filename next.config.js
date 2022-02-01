b3bd55f597c9ba6a5e23c938de9e2d4c9097d0cb
Daniel James <djames@alternativeairlines.com>
2022-02-01 20:29:26 +0000

    test adding vercel build env variables to config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
     reactStrictMode: true,
+    // Will be available on both server and client
+    publicRuntimeConfig: {
+        NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA,
+    }
 }
